da3531b411edb4c90a0e8942cfbd058d22e89b49
travisnava <trn5106@psu.edu>
2022-08-03 14:10:41 -0400

    Redeploying express api

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ const security = require("./middleware/security")
 const { NotFoundError } = require("./utils/errors")
 
 //middleware
-const cors= require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const cors= require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) //enable cross origin sharing
+//app.use(cors(corsOptions)) //enable cross origin sharing
+
+const cors = require("cors")
+app.use(cors())
+
 
 //create routes
 const authRoutes = require("./routes/auth")
